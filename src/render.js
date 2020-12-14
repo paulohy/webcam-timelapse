b58e79f5f4161dffac60c663a298d4adcb59b3f9
paulo.yamagata <yamagata@gatech.edu>
2020-12-13 16:47:48 -0800

    Added option to not display live video streaming

--- src/render.js
+++ src/render.js
@@ -5,6 +5,11 @@ const updateCameraListBtn = document.getElementById("updateCameraListBtn");
 updateCameraListBtn.onclick = updateCameraList;
 
 const video = document.getElementById("video");
+const imgPreview = document.getElementById("imgPreview");
+const previewBtn = document.getElementById("previewBtn");
+const liveCheck = document.getElementById("live")
+
+
 
 const remote = require("electron").remote;
 const app = remote.app;
@@ -39,18 +44,23 @@ function updateCameraList(){
 updateCameraList();
 
 
-let currentStream;
-var imageCapture;
+var previewStream;
+let previewCapture;
 
 function stopVideo(stream){
-    stream.getTracks().forEach(track=>{
+    previewStream.getTracks().forEach(track=>{
         track.stop();
     });
+    
+    video.hidden = true;
+    imgPreview.hidden = false;
+    previewBtn.disabled = false;
+    
 }
 
 function startVideo() {
-    if (typeof currentStream !== 'undefined'){
-        stopVideo(currentStream);
+    if (typeof previewStream !== 'undefined'){
+        stopVideo(previewStream);
     };
     
     let constraints = {}
@@ -75,19 +85,87 @@ function startVideo() {
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(stream=>{
-            currentStream = stream;
+            previewStream = stream;
             video.srcObject = stream; 
             
             const track = stream.getVideoTracks()[0];
-            imageCapture = new ImageCapture(track);
+            previewCapture = new ImageCapture(track);
         },
-        err => console.log(err));        
-
+        err => console.log(err));   
+        
+    video.hidden = false;
+    imgPreview.hidden = true;
+    previewBtn.disabled = true;
+    
 }
 
-cameraSelect.onchange = startVideo;
+function updateLivePreview(){
+    
+    if (liveCheck.checked){
+        startVideo();
+    }else{
+        stopVideo();
+    }
+}
 
-startVideo();
+
+function updatePreview(){
+    
+    let constraints = {}
+    
+    if (cameraSelect.value !== ""){
+        let videoConstraints = {
+            deviceId:{
+                exact: cameraSelect.value,
+            }
+        };
+        constraints = {
+            video: videoConstraints, 
+            audio: false
+        };
+    }else{
+        constraints = {
+            video: true, 
+            audio: false
+        };
+    }; 
+    
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(stream=>{           
+            previewStream = stream; 
+            const track = stream.getVideoTracks()[0];
+            previewCapture = new ImageCapture(track);
+            return previewCapture;
+            })
+        .then(capture=>{
+            return capture.takePhoto();            
+            })
+        .then(blob=>{
+            const url = URL.createObjectURL(blob);                    
+            imgPreview.src = url;
+            return previewStream;
+        })
+        .then(stream=>{
+            stream.getTracks().forEach(track=>{
+                track.stop();
+            });            
+        });
+
+    
+}
+
+
+
+previewBtn.onclick = updatePreview;
+
+liveCheck.onchange = updateLivePreview;
+
+
+
+
+
+// Time lapse
 
 
 let defaultPath = app.getPath("documents");
@@ -146,34 +224,79 @@ function TimerPause(){
 }
 
 function Snap(){
-    imageCapture.takePhoto().then(function(blob) {
-        
-        var reader = new FileReader();
-        
-        var filename = document.getElementById("fileNameRef").value;
-        
-        var dateNow = new Date(Date.now()).toString();
-        var dateFormat = moment(dateNow).format("YYYY-MM-DD_HH-mm-ss")
-        
-        filename += " - " +  dateFormat + ".jpeg";
-        
-        var path = node_path.join(targetPath,  filename);
-        
-        reader.onload = function(){
-            var buf = new Buffer(reader.result);
-            
-            fs.writeFile(path, buf, {}, (err,res) => {
-                if(err){
-                    console.error(err);
-                    return;
-                }                
+    
+    
+    let constraints = {}
+    
+    if (cameraSelect.value !== ""){
+        let videoConstraints = {
+            deviceId:{
+                exact: cameraSelect.value,
+            }
+        };
+        constraints = {
+            video: videoConstraints, 
+            audio: false
+        };
+    }else{
+        constraints = {
+            video: true, 
+            audio: false
+        };
+    }; 
+    
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(stream=>{           
+            previewStream = stream; 
+            const track = stream.getVideoTracks()[0];
+            previewCapture = new ImageCapture(track);
+            return previewCapture;
             })
-        }
+        .then(capture=>{
+            return capture.takePhoto();            
+            })
+        .then(blob=>{
+            
+            // update previwe with most recent photo
+            const url = URL.createObjectURL(blob);                    
+            imgPreview.src = url;
+            
+            
+            //save image
+            var reader = new FileReader();
         
-        reader.readAsArrayBuffer(blob);
-        
-        
-      });
+            var filename = document.getElementById("fileNameRef").value;
+            
+            var dateNow = new Date(Date.now()).toString();
+            var dateFormat = moment(dateNow).format("YYYY-MM-DD_HH-mm-ss")
+            
+            filename += " - " +  dateFormat + ".jpeg";
+            
+            var path = node_path.join(targetPath,  filename);
+            
+            reader.onload = function(){
+                var buf = new Buffer(reader.result);
+                
+                fs.writeFile(path, buf, {}, (err,res) => {
+                    if(err){
+                        console.error(err);
+                        return;
+                    }                
+                })
+            }
+            
+            reader.readAsArrayBuffer(blob);
+            
+            
+            return previewStream;
+        })
+        .then(stream=>{
+            stream.getTracks().forEach(track=>{
+                track.stop();
+            });            
+        });
+       
 }
 
 startBtn.onclick = TimerStart;
